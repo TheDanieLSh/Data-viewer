44a5ec60b03709a050ae221da120a4e1b94cc8be
Danila Shuvaev <dshuvaev@art3d.ru>
2024-06-21 16:43:48 +0300

    Backend initialized

---
+++ Data_viewer_backend/Data_viewer_backend/Modules/MainModule.cs
@@ -0,0 +1,32 @@
+﻿using Nancy;
+
+public class MainModule : NancyModule
+{
+    public MainModule()
+    {
+        Get("/", args => "Hello from Nancy!");
+
+        Post("/parsejson", args =>
+        {
+            var json = this.Request.Body.AsString();
+            return Response.AsText(json);
+        });
+
+        Post("/parsexml", args =>
+        {
+            var xml = this.Request.Body.AsString();
+            return Response.AsText(xml);
+        });
+    }
+}
+
+public static class Extensions
+{
+    public static string AsString(this Stream stream)
+    {
+        using (var reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+}
---
+++ Data_viewer_backend/Data_viewer_backend/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Nancy;
+using Nancy.Hosting.Self;
+
+var uri = new Uri("http://localhost:5000");
+using var host = new NancyHost(uri);
+host.Start();
+
+Console.WriteLine("Nancy is running on http://localhost:5000");
+Console.ReadLine();
+host.Stop();

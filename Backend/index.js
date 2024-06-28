03a51829e3c5c3c021cabe4eec2d76a5e80949a6
Danila Shuvaev <dshuvaev@art3d.ru>
2024-06-28 20:00:30 +0300

    wip

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,17 @@ app.use(router.allowedMethods());
 
 router.get('/', (context) => {
     context.response.body = 'Successfull response from server!'
+});
+
+router.post('/file_process', async (context) => {
+    const body = context.request.body({ 'type': 'text' });
+    // const link = await body.value;
+    console.log(body);
 })
 
+const HOST = 'localhost';
+const PORT = 4090;
 
-await app.listen({ port: 4090 });
+console.log('Deno server is running at ' + HOST + ':' + PORT);
+
+await app.listen({ hostname: HOST, port: PORT });

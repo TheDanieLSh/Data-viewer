a8aa235e1b7651111575598d0769f0c52dcc3301
Danila Shuvaev <dshuvaev@art3d.ru>
2024-07-01 17:59:19 +0300

    Backend configured

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { xml2js } from "https://deno.land/x/xml2js@1.0.0/mod.ts";
 
 const app = new Application();
 const router = new Router();
@@ -12,10 +13,42 @@ router.get('/', (context) => {
 });
 
 router.post('/file_process', async (context) => {
-    const body = context.request.body({ 'type': 'text' });
-    // const link = await body.value;
-    console.log(body);
+    const link = await context.request.body.text();
+    
+    let outputFile;
+
+    let isXML = false;
+
+    await fetch(link).then(resp => {
+        if (resp.headers.get('content-type').includes('application/json')) {
+
+            return resp.json();
+
+        } else if (resp.headers.get('content-type').includes('text/xml')) {
+
+            isXML = true;
+            return resp.text();
+
+        } else console.log('Unsupported type of the file');
+    }).then(result => {
+
+        if (isXML) {
+            const json = xml2js(result, { compact: true });
+            outputFile = JSON.stringify(json, null, '\t');
+
+        } else outputFile = result
+
+    });
+
+    console.log(outputFile);
+
+    if (!outputFile) {
+        context.response.body = 'Unsupported type of the file';
+    } else {
+        context.response.body = outputFile;
+    }
 })
+
 
 const HOST = 'localhost';
 const PORT = 4090;

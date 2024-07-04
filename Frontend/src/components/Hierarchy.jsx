cfff89666359a6943a64f1b6a089b8a1e32d91ae
Danila Shuvaev <dshuvaev@art3d.ru>
2024-07-04 17:24:32 +0300

    f5

--- Frontend/src/components/Hierarchy.jsx
+++ Frontend/src/components/Hierarchy.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'preact/hooks'
+import '../css/Hierarchy.scss'
+
+let levelHistory = [];
+
 export default function Hierarchy({ data }) {
-    const level = Object.keys(data).map(obj => obj);
+    const [curLvl, setCurLvl] = useState(null);
+
+    function levelParse(lvl) {
+        let result = [];
+
+        if (Array.isArray(lvl)) {
+            result = lvl.values.map(val => val);
+            result.forEach(el => {
+                if (typeof el === 'object') {
+
+                }
+            });
+        } else {
+            result = Object.keys(lvl).map(key => key);
+        }
+
+        return result
+    }
+
+    function goTo(e) {
+        console.log(e.target.innerHTML);
+    }
+
+    const firstLevel = levelParse(data);
 
     return (
         <div className="hierarchy">
-            {level.map(item => (
-                <div className="hierarchy__item">{item}</div>
+            {firstLevel.map(item => (
+                <div onClick={(e) => {goTo(e)}} className="hierarchy__item">{item}</div>
             ))}
         </div>
-    )
+    )  
 }

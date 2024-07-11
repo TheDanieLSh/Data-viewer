7df0a66e23add657afbd383d0d9ff6c36d39fe94
Danila Shuvaev <dshuvaev@art3d.ru>
2024-07-11 18:07:21 +0300

    Load animation added

--- Frontend/src/components/LinkField.jsx
+++ Frontend/src/components/LinkField.jsx
@@ -1,18 +1,24 @@
 ﻿import { dataSignal } from '../store.js'
+import { useState } from 'preact/hooks'
 import '../css/LinkField.scss'
 
 export default function LinkField() {
+    const [loading, setLoading] = useState(false);
 
-    const dataLoad = (e) => {
+    const dataLoad = async (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const link = formData.get('dataLink');
 
-        fetch('http://localhost:4090/file_process', {
+        setLoading(true);
+
+        await fetch('http://localhost:4090/file_process', {
             method: 'POST',
             headers: { 'Content-Type': 'text/plain' },
             body: link,
         }).then(resp => resp.json()).then(json => dataSignal.value = json);
+
+        setLoading(false);
     }
 
     return (
@@ -23,6 +29,7 @@ export default function LinkField() {
                 <input type="text" name="dataLink" />
                 <button>Загрузить</button>
             </form>
+            {loading && <div className='link-field__load-animation'></div>}
         </div>
     )
 }

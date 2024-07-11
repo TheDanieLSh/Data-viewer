eb5ed80ea7948c1dfb2597ac128a34be83e751e5
Danila Shuvaev <dshuvaev@art3d.ru>
2024-07-11 20:01:10 +0300

    f5

--- Frontend/src/components/Propertie.jsx
+++ Frontend/src/components/Propertie.jsx
@@ -16,7 +16,12 @@ export default function Propertie({ name, values }) {
             <div className='property__values' ref={valuesRef}>
                 {values.map(val => {
                     if (['string', 'number', 'boolean'].includes(typeof val))
-                        return <div className='property__val'>{val + ';'}</div>
+                        return (
+                            <>
+                                <div className='property__val'>{val}</div>
+                                <div className='property__separator'>;</div>
+                            </>
+                        )
                 })}
             </div>
             {isOverflow && <div className='property__expand'>...</div>}

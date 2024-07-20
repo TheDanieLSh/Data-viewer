4b146a5a5bcbd8be2df5b477f7692963bcd4a36e
TheDanieLSh <95643914+TheDanieLSh@users.noreply.github.com>
2024-07-20 08:35:18 +0300

    Object values display implementation

--- Frontend/src/components/Propertie.jsx
+++ Frontend/src/components/Propertie.jsx
@@ -46,18 +46,51 @@ export default function Propertie({ name, values, chosenValues }) {
         }
     }
 
+    const objValuesRestriction = {
+        '{...}': false,
+        '[...]': false,
+    }
+
     return (
         <div className={'property ' + (expanded && 'expanded')} ref={propertyRef}>
             <div className='property__name'>{name + ':'}</div>
             <div className='property__values' ref={valuesRef}>
                 {values.map(val => {
-                    if (['string', 'number', 'boolean'].includes(typeof val))
+                    if (['string', 'number', 'boolean'].includes(typeof val)) {
                         return (
                             <>
                                 <div className='property__val' onClick={(e) => {toggleValue(e)}}>{val}</div>
                                 <div className='property__separator'>;</div>
                             </>
                         )
+                    } else if (typeof val === null) {
+                        return (
+                            <>
+                                <div className='property__val' onClick={(e) => {toggleValue(e)}}>{'null'}</div>
+                                <div className='property__separator'>;</div>
+                            </>
+                        )
+                    } else if (typeof val === 'object') {
+                        const type = Array.isArray(val) ? '[...]' : '{...}';
+                        if (!objValuesRestriction[type]) {
+
+                            objValuesRestriction[type] = true;
+                            
+                            return (
+                            <>
+                                <div className='property__val'>{Array.isArray(val) ? '[...]' : '{...}'}</div>
+                                <div className='property__separator'>;</div>
+                            </>
+                        )
+                        }
+                    } else {
+                        return (
+                            <>
+                                <div className='property__val' onClick={(e) => {toggleValue(e)}}>{'!!!UNSUPPORTED DATA TYPE!!!'}</div>
+                                <div className='property__separator'>;</div>
+                            </>
+                        )
+                    }
                 })}
             </div>
             <div className='property__expand' onClick={(e) => {expandPropertie()}}>{overflow}</div>

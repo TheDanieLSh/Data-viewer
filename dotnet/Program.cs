5fbc24b92d095e7c9d77e95f2d477ec39b986390
Danila Shuvaev <dshuvaev@art3d.ru>
2024-08-05 19:05:00 +0300

    C# implementation added

---
+++ dotnet/Program.cs
@@ -0,0 +1,34 @@
+using System.Text.Json;
+using System.Net.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var HTTP = new HttpClient();
+
+app.MapGet("/", () => "Successfull response from server!");
+
+app.MapPost("/file_process", async (HttpRequest req) =>
+{
+    Console.WriteLine("Got a file process request");
+
+    bool isXML = false;
+
+    using var streamReader = new StreamReader(req.Body);
+
+    string link = await streamReader.ReadToEndAsync();
+
+    Console.WriteLine("Start fetching data");
+
+    HttpResponseMessage resp = await HTTP.GetAsync(link);
+
+    if (resp.Content.Headers.ContentType?.MediaType == "application/json")
+    {
+        string json = await resp.Content.ReadAsStringAsync();
+        return json;
+    }
+
+    return $"Failed to fetch {link}";
+});
+
+app.Run();
